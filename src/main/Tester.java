34be92cd44c9f91b49cbe1706c688dc1aa7b1633
Ylvakiller <rem.geuze@gmail.com>
2015-03-22 21:02:54 +0100

    Tested with marcel, code works on my pc

--- src/main/Tester.java
+++ src/main/Tester.java
@@ -1,13 +1,114 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
 public class Tester {
 
+	private final static String hostname = "jdbc:mysql://localhost/";
+	private final static String dbName = "databaseassignment";
+	private final static String username = "root";
+	private final static String password = "";
+	public static Connection con;
 	/**
 	 * This class is just to test individual classes
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
+		Tester.test();
 	}
 
+
+
+
+
+
+	private static void connect(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(hostname + dbName, username, password);
+		}catch(SQLException ex){
+			ex.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void test(){
+		Tester.connect();
+		String querry = "SELECT * FROM `test`";	
+		try{
+			Statement testStmnt = con.createStatement();
+			ResultSet testRss = testStmnt.executeQuery(querry);
+			testRss.next();
+			int temp = testRss.getInt(1);
+			System.out.println(temp);
+			Tester.close();
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static void close() {
+		try {
+			con.close();
+		}catch (SQLException ex){
+			ex.printStackTrace();
+		}
+
+	}
 }
+
+
+
+
+
+
+/*
+import java.sql.*;
+
+
+public class DBConnect {
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+
+
+    public DBConnect(){
+        try{
+           Class.forName("com.mysql.jdbc.Driver"); 
+
+           con = DriverManager.getConnection("jdbc:mysql://localhost/test","root","");
+           //st = con.createStatement();
+
+        }catch(Exception ex){
+          System.out.println("Error:" +ex);  
+          ex.printStackTrace();
+        }
+    }
+
+    /*
+    public void getData(){
+        try{
+
+            String query = "select * from Users";
+            rs = st.executeQuery(query);
+            System.out.println("Records from Database");
+            while(rs.next()){
+                String name = rs.getString("UserName");
+                //String password = rs.getString("Password");
+                System.out.println("Name: "+ name);
+
+            }
+
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
+    }
+ */
